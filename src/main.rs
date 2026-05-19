3a6df2a633027ca02949a16ca0063a9ede4cd0c8
Enzo Persillet (Tutez) <enzopersillet@gmail.com>
2026-05-19 20:39:43 +0200

    Switch to build.rs

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,7 @@
 // when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-slint::slint!{
-    export component HelloWorld {
-        Text {
-            text: "hello world";
-            color: orange;
-        }
-    }
-}
+slint::include_modules!();
 
 fn main() {
     HelloWorld::new().unwrap().run().unwrap();
